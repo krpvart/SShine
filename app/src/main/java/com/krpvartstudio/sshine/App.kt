4238d0e4f120c4200e7e0e017970a1885c6d4c12
krpvart <nskwin32cih@gmail.com>
2021-09-14 10:24:40 +0700

    Добавлен класс Apt.kt

---
+++ app/src/main/java/com/krpvartstudio/sshine/App.kt
@@ -0,0 +1,24 @@
+package com.krpvartstudio.sshine
+
+import android.app.Application
+import android.content.Intent
+const val APP_SETTINGS = "App settings"
+const val IS_STARTED_UP = "Is started up"
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val preferences = getSharedPreferences(APP_SETTINGS, MODE_PRIVATE)
+        val flag = preferences.contains(IS_STARTED_UP)
+
+        if(!flag){
+            val editor = preferences.edit()
+            editor.putBoolean(IS_STARTED_UP,true)
+            editor.apply()
+            val intent = Intent(this,InitialActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+
+        }
+    }
+}
